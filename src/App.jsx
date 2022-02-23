65f593065df9a7d0eac5dcfcc0b1dbc11fa892d5
gustave ploix <gustaveploix.pro@gmail.com>
2022-02-23 17:10:12 +0100

    added some elements to the applicaiton

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,21 @@
-import React from 'react';
+import React, { useState } from 'react';
+
+import ToggleBtn from './components/ToggleBtn';
 
 import './styles.scss';
 
 export default function App() {
 
+    const [light, setLight] = useState(false);
 
     return (
-        <h1>Hello world</h1>
-    );
+    
+        <div className={light ? 'fullPage light' : 'fullPage dark'}>
+            {/* <ToggleBtn 
+                light={light}
+                setLight={setLight}
+            /> */}
+        </div>
+        
+        );
 };
